9f2f7d2c6d0a3d737585fec0a19fb92f31792bf2
XY_cpp <xy_cpp@icloud.com>
2024-08-17 21:23:48 +0800

    [feat]修改提交

--- src/pages/Editor/ProblemEditor.vue
+++ src/pages/Editor/ProblemEditor.vue
@@ -10,25 +10,35 @@
         </el-row>
         <el-row>
             <el-col :span="24">
-                <h5>时间复杂度：单位MS</h5>
+                <h5>时间限制：单位MS</h5>
                 <el-input v-model="timelimit" placeholder="时间复杂度" />
 
-                <h5>空间复杂度：单位MB</h5>
+                <h5>空间限制：单位MB</h5>
                 <el-input v-model="memorylimit" placeholder="空间复杂度" />
 
-                <el-button type="primary" @click="Submit()">提交</el-button>
+                <h5>上传测试点</h5>
+                <el-upload accept=".zip" :on-change="UploadOnChange" :auto-upload="false" :show-file-list="false">
+                    <el-input :placeholder="file_name" slot="trigger" readonly />
+                </el-upload>
+
             </el-col>
         </el-row>
+        <el-row>
+            <el-col :span="24">
+
+            </el-col>
+        </el-row>
+        <el-button type="primary" @click="Submit()">提交</el-button>
     </el-card>
 </template>
 
 <script lang="ts" setup>
-import {  reactive, ref ,onMounted} from 'vue'
+import { reactive, ref, onMounted } from 'vue'
 import MarkDownEditor from '../../components/MarkDownEditor.vue'
 import store from '../../store'
 import { ElMessage } from 'element-plus'
 import service from '../../axios'
-import { useRouter,useRoute } from 'vue-router'
+import { useRouter, useRoute } from 'vue-router'
 
 const router = useRouter()
 const route = useRoute()
@@ -45,10 +55,11 @@ const timelimit = ref(2000)
 const memorylimit = ref(128)
 const uid = ref(0)
 const judgenum = ref(0)
+const file_name = ref('.zip')
 
-interface TestInfo{
-    in : string
-    out : string
+interface TestInfo {
+    in: string
+    out: string
 }
 interface ProblemData {
     pid: number;
@@ -60,16 +71,27 @@ interface ProblemData {
     uid: number;
 }
 
+function UploadOnChange(file, _) {
+    file_name.value = file.name
+    const formData = new FormData();
+    formData.append('file', file.raw);
+    formData.append('pid',problemid.value)
+    service.post('/api/problem/upload', formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    })
+}
 
-function Submit()
-{
-    if(title.value.length < 2){
+
+function Submit() {
+    if (title.value.length < 2) {
         pointmessage.value = "题目标题太短！"
         WaringMessage()
         return
     }
 
-    const data=<ProblemData>{}
+    const data = <ProblemData>{}
     data.pid = Number(problemid.value)
     data.title = title.value
     data.description = markdowneditor.value.GetContent()
@@ -78,118 +100,116 @@ function Submit()
     data.memory_limit = memorylimit.value
     data.uid = uid.value
 
-    if(edittype.value == "Update"){
+    if (edittype.value == "Update") {
         console.log("修改！！！")
         service
-        .post(`/api/problem/update`, {
-            pid: data.pid,
-            title: data.title,
-            description: data.description,
-            judge_num: data.judge_num,
-            time_limit: data.time_limit,
-            memory_limit: data.memory_limit,
-            uid: data.uid
-        })
-        .then(
-        (response) => {
-            let json = response.data
-            console.log("response:", json);
-            if(json.status == "success"){
-                console.log("提交成功了！！！", json);
-                pointmessage.value = "题目插入成功！"
-                SuccessMessage()
-                router.go(-1)
-            }else{
-                pointmessage.value = json.message
-                WaringMessage()
-                return
-            }
-        },
-        (error) => {
-            console.log(error.data);
-            pointmessage.value = "提交失败,可能网络出现问题！"
-            ErrorMessage()
-            return
-        }
-    )
-    }else if(edittype.value == "Insert"){
+            .post(`/api/problem/update`, {
+                pid: data.pid,
+                title: data.title,
+                description: data.description,
+                judge_num: data.judge_num,
+                time_limit: data.time_limit,
+                memory_limit: data.memory_limit,
+                uid: data.uid
+            })
+            .then(
+                (response) => {
+                    let json = response.data
+                    console.log("response:", json);
+                    if (json.status == "success") {
+                        console.log("提交成功了！！！", json);
+                        pointmessage.value = "题目插入成功！"
+                        SuccessMessage()
+                        router.go(-1)
+                    } else {
+                        pointmessage.value = json.message
+                        WaringMessage()
+                        return
+                    }
+                },
+                (error) => {
+                    console.log(error.data);
+                    pointmessage.value = "提交失败,可能网络出现问题！"
+                    ErrorMessage()
+                    return
+                }
+            )
+    } else if (edittype.value == "Insert") {
         console.log("提交！！！")
         service
-        .post(`/api/problem/insert`  , {
-            title: data.title,
-            description: data.description,
-            judge_num: data.judge_num,
-            time_limit: data.time_limit,
-            memory_limit: data.memory_limit,
-            uid: data.uid
-        })
-        .then(
-        (response) => {
-            let json = response.data
-            if(json.status == "success"){
-                console.log("提交成功了！！！", json);
-                pointmessage.value = "题目插入成功！"
-                SuccessMessage()
-                router.go(-1)
-            }else{
-                pointmessage.value = json.message
-                WaringMessage()
-                return
-            }
-        },
-        (error) => {
-            console.log(error.data);
-            pointmessage.value = "提交失败,可能网络出现问题！"
-            ErrorMessage()
-            return
-        }
-    )
+            .post(`/api/problem/insert`, {
+                title: data.title,
+                description: data.description,
+                judge_num: data.judge_num,
+                time_limit: data.time_limit,
+                memory_limit: data.memory_limit,
+                uid: data.uid
+            })
+            .then(
+                (response) => {
+                    let json = response.data
+                    if (json.status == "success") {
+                        console.log("提交成功了！！！", json);
+                        pointmessage.value = "题目插入成功！"
+                        SuccessMessage()
+                        router.go(-1)
+                    } else {
+                        pointmessage.value = json.message
+                        WaringMessage()
+                        return
+                    }
+                },
+                (error) => {
+                    console.log(error.data);
+                    pointmessage.value = "提交失败,可能网络出现问题！"
+                    ErrorMessage()
+                    return
+                }
+            )
     }
 
 }
-function GetServerInfo()
-{   
+function GetServerInfo() {
     service
         .post(`/api/problem/query`, {
             pid: Number(problemid.value)
         })
         .then(
-        (response) => {
-            let json = response.data
-            if(json.status == "success"){
-                console.log('题目获取成功！', json)
-                pointmessage.value = "题目获取成功！"
-                SuccessMessage()
-                handledata(json.data)
-            }else{
-                pointmessage.value = json.message
-                WaringMessage()
+            (response) => {
+                let json = response.data
+                if (json.status == "success") {
+                    console.log('题目获取成功！', json)
+                    pointmessage.value = "题目获取成功！"
+                    SuccessMessage()
+                    handledata(json.data)
+                } else {
+                    pointmessage.value = json.message
+                    WaringMessage()
+                    return
+                }
+            },
+            (error) => {
+                console.log(error.data);
+                pointmessage.value = "提交失败,可能网络出现问题！"
+                ErrorMessage()
                 return
             }
-        },
-        (error) => {
-            console.log(error.data);
-            pointmessage.value = "提交失败,可能网络出现问题！"
-            ErrorMessage()
-            return
-        }
-    )
+        )
 }
 //Json(Result,Reason,pid,title,description,time_limit,memory_limit,uid)
-function handledata(Info:any)
-{
+function handledata(Info: any) {
     title.value = Info.title
     markdowneditor.value.SetContent(Info.description)
     timelimit.value = Number(Info.time_limit)
     memorylimit.value = Number(Info.memory_limit)
     uid.value = Number(Info.uid)
 }
-onMounted(()=>{
+onMounted(() => {
     problemid.value = route.query.pid
     edittype.value = String(route.query.edittype)
-    if(edittype.value == "Update"){ // 如果是更新，需要向服务器获取数据,并更新数据
+    if (edittype.value == "Update") { // 如果是更新，需要向服务器获取数据,并更新数据
         GetServerInfo()
-    }else if(edittype.value == "Insert"){ // 否则是添加
+    } else if (edittype.value == "Insert") { // 否则是添加
         uid.value = store.state.uid
     }
 })
@@ -221,19 +241,23 @@ const ErrorMessage = () => {
 
 <style lang="scss">
 .el-row {
-  margin-bottom: 20px;
+    margin-bottom: 20px;
 }
+
 .el-row:last-child {
-  margin-bottom: 0;
+    margin-bottom: 0;
 }
+
 .el-col {
-  border-radius: 4px;
+    border-radius: 4px;
 }
-.box-card{
+
+.box-card {
     margin: 10px;
 }
+
 .grid-content {
-  border-radius: 4px;
-  min-height: 36px;
+    border-radius: 4px;
+    min-height: 36px;
 }
 </style>
